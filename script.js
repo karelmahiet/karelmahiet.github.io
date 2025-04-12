e334c6778bfedc69044b817ee5d0e2ccd8cd41eb
karelmahiet <karelmahiet@yahoo.com>
2025-04-11 20:08:37 -0400

    fix videos

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ function toggleMenu() {
 }
 
 const video = document.getElementById('myVideo');
+const video2 = document.getElementById('myVideo2');
 const playFullscreenButton = document.getElementById('playFullScreen');
+const playFullscreenButton2 = document.getElementById('playFullScreen2');
 
+//luigi
 playFullscreenButton.addEventListener('click', () => {
   video.style.display = 'block';
   video.play();
@@ -38,5 +41,41 @@ document.addEventListener('msfullscreenchange', () => {
   if (!document.msFullscreenElement) {
     video.pause();
     video.style.display = 'none';
+  }
+});
+
+
+//genimon
+playFullscreenButton2.addEventListener('click', () => {
+  video2.style.display = 'block';
+  video2.play();
+  if (video2.requestFullscreen) {
+    video2.requestFullscreen();
+  } else if (video2.webkitRequestFullscreen) {
+    video2.webkitRequestFullscreen();
+  } else if (video2.msRequestFullscreen) {
+    video2.msRequestFullscreen();
+  }
+});
+
+
+document.addEventListener('fullscreenchange', () => {
+  if (!document.fullscreenElement) {
+    video2.pause();
+    video2.style.display = 'none';
+  }
+});
+
+document.addEventListener('webkitfullscreenchange', () => {
+  if (!document.webkitFullscreenElement) {
+    video2.pause();
+    video2.style.display = 'none';
+  }
+});
+
+document.addEventListener('msfullscreenchange', () => {
+  if (!document.msFullscreenElement) {
+    video2.pause();
+    video2.style.display = 'none';
   }
 });
